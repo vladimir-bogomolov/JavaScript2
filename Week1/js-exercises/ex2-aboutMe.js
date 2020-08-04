a7afe7ac39063339765e5acc91a9d5530c72c840
Vladimir <ecofovka@gmail.com>
2020-08-04 19:03:02 +0200

    completed exercise 2

--- Week1/js-exercises/ex2-aboutMe.js
+++ Week1/js-exercises/ex2-aboutMe.js
@@ -9,3 +9,21 @@
  5. See HTML
  6. Create a new img element and set its src attribute to a picture of you.Append that element to the page.
  */
+
+ document.body.style.fontFamily = 'Arial, sans-serif';
+ let nickname = document.createTextNode('Vladimir');
+ document.getElementById('nickname').appendChild(nickname);
+ let favFood = document.createTextNode('Grilled Tuna');
+ document.getElementById('fav-food').appendChild(favFood);
+ let homeTown = document.createTextNode('Amsterdam');
+ document.getElementById('hometown').appendChild(homeTown);
+ let listItems = document.querySelectorAll('li');
+ Array.from(listItems).forEach(item => item.className = 'list-item');
+//  Should I make step 5 here or in HTML?
+// let styleTag = document.createElement('style');
+// styleTag.innerHTML = '.list-item {color: red;}';
+// document.head.appendChild(styleTag);
+let myPicture = document.createElement('img');
+myPicture.src = 'myPicture.png';
+myPicture.style.width = '150px';
+document.body.appendChild(myPicture);
