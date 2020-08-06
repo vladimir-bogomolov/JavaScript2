eb0da01462bf6d3068860223af4adeb207ce528a
Vladimir <ecofovka@gmail.com>
2020-08-06 11:56:00 +0200

    project completed

--- Week1/project/index.js
+++ Week1/project/index.js
@@ -1,1 +1,35 @@
 // your code goes in here
+function getQuote() {
+    let quotes = [{
+        author: 'Oscar Wilde',
+        quote: 'Be yourself, everyone else is already taken.'
+    }, 
+    {
+        author: 'Frank Zappa',
+        quote: 'So many books, so little time.'
+    },
+    {
+        author: 'Allen Saunders',
+        quote: 'Life is what happens to us while we are making other plans.'
+    },
+    {
+        author: 'Thomas A. Edison',
+        quote: 'I have not failed. I\'ve just found 10,000 ways that won\'t work.'
+    },
+    {
+        author: 'Friedrich Nietzsche',
+        quote: 'That which does not kill us makes us stronger.'
+    },
+    {
+        author: 'George Eliot',
+        quote: 'It is never too late to be what you might have been.'
+    }];
+
+    let currentQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+    document.getElementById('quote').innerHTML = '<i class="fas fa-quote-left"></i>' + currentQuote.quote;
+    document.getElementById('author').innerHTML = '- ' + currentQuote.author;
+
+}
+
+document.getElementById('newQuote').addEventListener('click', getQuote);
