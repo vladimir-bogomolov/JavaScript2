2c8a0378d95e5db0dda87de14c9d9535b60f0a7b
Vladimir <ecofovka@gmail.com>
2020-08-12 11:45:17 +0200

    lemonAllergy is done

--- Week2/js-exercises/ex3-lemonAllergy.js
+++ Week2/js-exercises/ex3-lemonAllergy.js
@@ -14,6 +14,8 @@
 
 function takeOutLemons(basket) {
   // your code goes in here. The output is a string 
+  basket = basket.filter(fruit => fruit.toLowerCase() !== 'lemon');
+  return 'My mom bought me a fruit basket, containing ' + basket.reduce((result, fruit) => result + fruit + ', ', '').slice(0, -2) + '!';
 }
 
 const fruitBasket = ['Apple', 'Lemon', 'Grapefruit', 'Lemon', 'Banana', 'Watermelon', 'Lemon'];
